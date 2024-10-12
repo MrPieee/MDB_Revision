c5bf96845bea3cc52833a416427be2c946220f44
Abu Sayed <abus04651@gmail.com>
2024-10-12 23:35:04 +0600

    Make CRUD app with mongo in express

---
+++ validatorAndOperator/app.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const cors=require('cors');
+const userRouter = require('./router/user.route');
+
+const app = express();
+
+// Middleware function call 
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
+
+// Home route
+app.get('/',(req,res)=>{
+    try {
+        return res.status(200).sendFile(__dirname+'/view/index.html');
+    } catch (error) {
+       return res.status(500).json({message:"Something broke",error:error.message}); 
+    } 
+});
+
+// User route
+app.use('/api',userRouter);
+
+// false route
+app.use((req,res,next)=>{
+    return res.status(404).json({success:false, message:`The route wasn't found`});
+});
+
+
+module.exports=app;
